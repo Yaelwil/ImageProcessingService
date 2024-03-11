7f4fcb56e5afb7e3b28f030d0eada1fb26b30cc6
YaelWil <yaelwilamo@gmail.com>
2024-03-11 18:39:13 +0200

    DRAFT

--- polybot/responses.py
+++ polybot/responses.py
@@ -2,11 +2,6 @@ import json
 
 
 def load_responses():
-    file_path = './responses.json'
-    with open(file_path, 'r') as file:
-        try:
-            responses_data = json.load(file)
-            return responses_data
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-            return None
+    with open('./responses.json', 'r') as file:
+        return json.load(file)
+
